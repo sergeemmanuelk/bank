df5b64944db379369ba12542b5d22aa83357ad74
sergeemmanuelk <serge_em@outlook.fr>
2025-08-01 18:39:32 +0000

    fix: throw an exception in case of duplicate when creating a user with an address that already exists

--- bank-api/src/main/java/dev/skonan/bank/handlers/GlobalExceptionHandler.java
+++ bank-api/src/main/java/dev/skonan/bank/handlers/GlobalExceptionHandler.java
@@ -3,6 +3,7 @@ package dev.skonan.bank.handlers;
 import dev.skonan.bank.exceptions.ObjectValidationException;
 import dev.skonan.bank.exceptions.OperationNonPermittedException;
 import jakarta.persistence.EntityNotFoundException;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -40,6 +41,16 @@ public class GlobalExceptionHandler {
                 .build();
         return ResponseEntity
                 .status(HttpStatus.NOT_ACCEPTABLE)
+                .body(representation);
+    }
+
+    @ExceptionHandler(DataIntegrityViolationException.class)
+    public ResponseEntity<ExceptionRepresentation> handleException() {
+        ExceptionRepresentation representation = ExceptionRepresentation.builder()
+                .errorMessage("A user already exists with the provided Email")
+                .build();
+        return ResponseEntity
+                .status(HttpStatus.BAD_REQUEST)
                 .body(representation);
     }
 }
