706b97eb81b6d7850e4c8da17f6b43c5924dff2a
sergeemmanuelk <serge_em@outlook.fr>
2025-08-01 16:24:06 +0000

    implement statistics controller

---
+++ bank-api/src/main/java/dev/skonan/bank/controllers/StatisticsController.java
@@ -0,0 +1,50 @@
+package dev.skonan.bank.controllers;
+
+import dev.skonan.bank.dtos.TransactionSumDetails;
+import dev.skonan.bank.services.StatisticsService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.List;
+
+@RestController
+@RequestMapping("/statistics")
+@RequiredArgsConstructor
+public class StatisticsController {
+
+    private final StatisticsService statisticsService;
+
+    @GetMapping("/sum-by-date/{user-id}")
+    public ResponseEntity<List<TransactionSumDetails>> findSumTransactionsByDate(
+            @PathVariable("user-id") Integer userId,
+            @RequestParam("start-date") @DateTimeFormat(pattern = "yyyy-MM-dd") LocalDate startDate,
+            @RequestParam("end-date")  @DateTimeFormat(pattern = "yyyy-MM-dd") LocalDate endDate
+    ){
+        return ResponseEntity.ok(statisticsService.findSumTransactionsByDate(startDate, endDate, userId));
+    }
+
+    @GetMapping("/account-balance/{user-id}")
+    public ResponseEntity<BigDecimal> getAccountBalance(
+            @PathVariable("user-id") Integer userId
+    ){
+        return ResponseEntity.ok(statisticsService.getAccountBalance(userId));
+    }
+
+    @GetMapping("/highest-transfer/{user-id}")
+    public ResponseEntity<BigDecimal> highestTransfer(
+            @PathVariable("user-id") Integer userId
+    ){
+        return ResponseEntity.ok(statisticsService.highestTransfer(userId));
+    }
+
+    @GetMapping("/highest-deposit/{user-id}")
+    public ResponseEntity<BigDecimal> highestDeposit(
+            @PathVariable("user-id") Integer userId
+    ){
+        return ResponseEntity.ok(statisticsService.highestDeposit(userId));
+    }
+}
