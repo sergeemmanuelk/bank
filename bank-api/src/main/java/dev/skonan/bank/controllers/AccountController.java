9c7d46335087f60d36c8fcb34c0d214dce9e9fd7
sergeemmanuelk <serge_em@outlook.fr>
2025-08-01 16:13:51 +0000

    implement account controller

---
+++ bank-api/src/main/java/dev/skonan/bank/controllers/AccountController.java
@@ -0,0 +1,44 @@
+package dev.skonan.bank.controllers;
+
+import dev.skonan.bank.dtos.AccountDto;
+import dev.skonan.bank.services.AccountService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/accounts")
+@RequiredArgsConstructor
+public class AccountController {
+
+    private final AccountService accountService;
+
+    @PostMapping("/")
+    public ResponseEntity<Integer> save(
+            @RequestBody AccountDto accountDto
+    ) {
+        return ResponseEntity.ok(accountService.save(accountDto));
+    }
+
+    @GetMapping("/")
+    public ResponseEntity<List<AccountDto>> findAll() {
+        return ResponseEntity.ok(accountService.findAll());
+    }
+
+    @GetMapping("/{account-id}")
+    public ResponseEntity<AccountDto> findById(
+            @PathVariable("account-id") Integer accountId
+    ) {
+        return ResponseEntity.ok(accountService.findById(accountId));
+    }
+
+    @DeleteMapping("/{account-id}")
+    public ResponseEntity<Void> delete(
+            @PathVariable("account-id") Integer accountId
+    ) {
+        accountService.delete(accountId);
+        return ResponseEntity.accepted().build();
+    }
+}
